899e93079f5b9444152c831e5957f1b4fe91279f
Victor <116448381+ReinVDev@users.noreply.github.com>
2023-02-06 15:48:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+function App() {
+ 
+
+  return (
+  <div className='App'>
+    <Navbar />
+    <Outlet />  
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+import './Navbar.css'
+import { useState } from "react";
+const Navbar = () => {
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!search) return 
+    navigate(`/search?q=${search}`)
+    setSearch("")
+  }
+  return (
+    <div>
+      <nav id="navbar">
+        <h2>
+          <Link to="/">
+            <BiCameraMovie /> Movies Lib
+          </Link>
+        </h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="Busque um filme" onChange={(e) => setSearch(e.target.value)}value={search}/>
+          <button type="submit">
+            <BiSearchAlt2 />
+          </button>
+        </form>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
