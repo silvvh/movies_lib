149c6151d939a9238b8e3115bed75ef441831bbd
Victor <116448381+ReinVDev@users.noreply.github.com>
2023-02-06 17:44:24 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 
   return (
     <div className="container">
-      <h2 className="title">Melhores filmes:</h2>
+      <h2 className="topmovies">Melhores Filmes</h2>
       <div className="movies-container">
         {topMovies.lenght === 0 && <p>Carregando...</p>}
         {topMovies.length > 0 &&
